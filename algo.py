a5243eefd993d9812e2f50f9fe9f7d3ad77696c7
Tim Garvin <tcgarvin@gmail.com>
2016-12-29 01:14:50 -0500

    Tweaked to make sure we're invested to the hilt.
    Retries buys and sells until we get what we're targeting.
    
    Also reorganized a bit of the analysis, putting it all together.
    
    It seems a lot slower now, but I think that's because of needing to re-issue orders all the time. I don't well understand how that works.

--- algo.py
+++ algo.py
@@ -15,12 +15,14 @@
 import pandas as pd
 import numpy as np
 
+# Called by framework
 def initialize(context):
     context.max_num_stocks = 50
     context.days = 0
     context.quarter_days = 65
     context.relative_strength_6m = {}
-    schedule_function(func=compute_strength_and_rebalance, date_rule=date_rules.every_day())
+    context.target_positions = {}
+    schedule_function(func=reconcile_target_positions, date_rule=date_rules.every_day())
           
 def quarter_passed(context): 
     """
@@ -28,6 +30,7 @@ def quarter_passed(context):
     """
     return context.days % context.quarter_days == 0
 
+# Called by framework
 def before_trading_start(context, data): 
     context.days += 1
     
@@ -36,6 +39,8 @@ def before_trading_start(context, data):
     
     do_screening(context)
     context.security_list = list(context.fundamental_df.columns.values)
+    compute_relative_strength(context, data)
+    rebalance(context, data)
     
 def do_screening(context):
     fundamental_df = get_fundamentals(
@@ -61,7 +66,7 @@ def do_screening(context):
         # No Financials (103) and Real Estate (104) Stocks, no ADR or PINK, only USA
         .filter(fundamentals.asset_classification.morningstar_sector_code != 103)
         .filter(fundamentals.company_reference.country_id == "USA")
-        .filter(fundamentals.asset_classification. != 104)
+        .filter(fundamentals.asset_classification.morningstar_sector_code != 104)
         .filter(fundamentals.share_class_reference.is_depositary_receipt == False)
         .filter(fundamentals.share_class_reference.is_primary_share == True)
         .filter(fundamentals.company_reference.primary_exchange_id != "OTCPK")
@@ -95,10 +100,7 @@ def rebalance(context, data):
         Buy all stocks equally 
     """
     # Exit all positions before starting new ones
-    for stock in context.portfolio.positions:
-        if stock not in context.fundamental_df:
-            if data.can_trade(stock):
-                order_target_percent(stock, 0)
+    context.target_positions = {}
 
     # Filter out stocks without data and apply the momentum criteria
     # -0.6745 is an approximation for the top three-quarters of the market
@@ -111,16 +113,35 @@ def rebalance(context, data):
    
     weight = 1.0/len(context.stocks)
 
-    log.info("Ordering %0.0f%% for each of %s (%d stocks)" % (weight * 100, ', '.join(stock.symbol for stock in context.stocks), len(context.stocks)))
+    log.info("Targeting %0.0f%% for each of %s (%d stocks)" % (weight * 100, ', '.join(stock.symbol for stock in context.stocks), len(context.stocks)))
     
     # buy all stocks equally
     for stock in context.stocks:
         if data.can_trade(stock):
-            order_target_percent(stock, weight)
+            context.target_positions[stock] = TargetPosition(stock, weight)
+        else:
+            log.error("This shouldn't happen (%s)" % stock.symbol)
 
     # track how many positions we're holding
-    record(num_positions = len(context.fundamental_df))
+    record(num_positions = len(context.target_positions))
     
+# Actually places orders, trying to get us where we want to be
+def reconcile_target_positions(context, data):
+    # First, sell off anything not being targeted
+    selling = []
+    for stock in context.portfolio.positions:
+        if stock not in context.target_positions:
+            if data.can_trade(stock):
+                order_target_percent(stock, 0)
+                selling.append(stock.symbol)
+                
+    if len(selling) > 0:
+        log.info("Trying to sell: %s" % ", ".join(selling))
+                
+    # Then go through and try to meet the targets we've set.
+    for target in context.target_positions.itervalues():
+        if (data.can_trade(target.stock)):
+            order_target_percent(target.stock, target.percent)
     
 def compute_relative_strength(context, data):   
     prices = data.history(context.security_list + [symbol('SPY')], 'price', 150, '1d')
@@ -133,11 +154,9 @@ def compute_relative_strength(context, data):
         pct_change = pct_change / abs(pct_change_spy)
     pct_change = pct_change.drop(symbol('SPY'))
     context.relative_strength_6m = pct_change
-
-def compute_strength_and_rebalance(context, data):
-    if not quarter_passed(context):
-        return
     
-    compute_relative_strength(context, data)
-    rebalance(context, data)  
+class TargetPosition:
+    def __init__(self, stock, percent):
+        self.stock = stock
+        self.percent = percent
 
