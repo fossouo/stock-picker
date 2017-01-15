cba6503f576d190ad1bef6fe5eb2169af6e1028a
Tim Garvin <tcgarvin@gmail.com>
2017-01-15 01:18:54 -0500

    Trying not to spend more cash than we have available.

--- algo.py
+++ algo.py
@@ -12,6 +12,7 @@
     The positions are updated quarterly.
 """
 
+import math
 import pandas as pd
 import numpy as np
 
@@ -113,37 +114,68 @@ def rebalance(context, data):
         log.info("No Stocks to buy")
         return
    
-    weight = 1.0/len(context.stocks)
+    division = min(1, 1.0/len(context.stocks))
+    total_value = context.portfolio.portfolio_value
+    value_per_division = total_value * division
 
-    log.info("Targeting %0.0f%% for each of %s (%d stocks)" % (weight * 100, ', '.join(stock.symbol for stock in context.stocks), len(context.stocks)))
+    log.info("Current Value: %0.f Targeting position of $%0.f for each of %s (%d stocks)" % (total_value, value_per_division, ', '.join(stock.symbol for stock in context.stocks), len(context.stocks)))
     
     # buy all stocks equally
     for stock in context.stocks:
         if data.can_trade(stock):
-            context.target_positions[stock] = TargetPosition(stock, weight)
+            stock_price = data.current(stock, "price")
+            number_shares = math.floor(value_per_division / stock_price)
+            context.target_positions[stock] = TargetPosition(stock, number_shares)
         else:
             log.error("This shouldn't happen (%s)" % stock.symbol)
-
+            
     # track how many positions we're holding
     record(num_positions = len(context.target_positions))
     
 # Actually places orders, trying to get us where we want to be
 def reconcile_target_positions(context, data):
+    record(cash_balance = context.portfolio.cash)
+    
     # First, sell off anything not being targeted
     selling = []
     for stock in context.portfolio.positions:
         if stock not in context.target_positions:
             if data.can_trade(stock):
-                order_target_percent(stock, 0)
+                order_target(stock, 0)
                 selling.append(stock.symbol)
                 
     if len(selling) > 0:
         log.info("Trying to sell: %s" % ", ".join(selling))
                 
-    # Then go through and try to meet the targets we've set.
+    # Then go through and try to meet the targets we've set.  We need to manually keep track of what cash is on hand so we don't over-reach
+    cash_available = context.portfolio.cash
+    can_invest_extra_cash = True
     for target in context.target_positions.itervalues():
-        if (data.can_trade(target.stock)):
-            order_target_percent(target.stock, target.percent)
+        existing_share_count = context.portfolio.positions[target.stock].amount
+        desired_share_count = target.number_shares
+        share_count_difference = desired_share_count - existing_share_count
+        
+        # If we have more to buy, we can't backfill
+        if share_count_difference > 0:
+            can_invest_extra_cash = False
+            order_id, cash_spent = cash_sensitive_order_target(target.stock, target.number_shares, context, data, cash_available)
+            cash_available -= cash_spent
+        elif share_count_difference < 0:
+            log.info("Selling because %d - %d = %d" % (existing_share_count, desired_share_count, share_count_difference))
+            order_target(target.stock, target.number_shares)
+            
+# Like order_target, but tries not to spend more than the given amount.
+# Returns the order id and an estimate of cash spent
+def cash_sensitive_order_target(stock, number_shares, context, data, cash_available):
+    order_id = None
+    cash_spent = 0
+    share_price = data.current(stock, "price")
+    shares_to_buy = min(number_shares, math.floor(float(cash_available) / share_price))
+    if (data.can_trade(stock)):
+        order_id = order(stock, shares_to_buy)    
+        cash_spent = share_price * shares_to_buy
+        
+    return order_id, cash_spent
     
 def compute_relative_strength(context, data):   
     prices = data.history(context.security_list + [symbol('SPY')], 'price', 150, '1d')
@@ -158,7 +190,7 @@ def compute_relative_strength(context, data):
     context.relative_strength_6m = pct_change
     
 class TargetPosition:
-    def __init__(self, stock, percent):
+    def __init__(self, stock, number_shares):
         self.stock = stock
-        self.percent = percent
+        self.number_shares = number_shares
 
